554de3572466fa49cf18c2ff87e3b01d99960444
Pedro Tiussi <pedrotiussi@gmail.com>
2020-03-21 17:23:25 -0300

    O location.href do cadastrar não está funcionando e eu não consegui ver o erro

---
+++ bdconfig/bdconfig.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE usuario(
+	user_id int,
+	name varchar (15),
+	password varchar (20),
+	PRIMARY KEY (user_id)
+	);
+
+CREATE TABLE message (
+	message_id int,
+	sender int references usuario (user_id),
+	receiver int references usuario (user_id),
+	message_text varchar (100),
+	message_time timestamp,
+	PRIMARY KEY (message_id)
+	);
+
