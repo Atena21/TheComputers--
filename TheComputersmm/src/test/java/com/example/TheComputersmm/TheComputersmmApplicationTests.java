8cd27f84337a4053a27d44622568651058874774
Leonardo Gomes <leonardo.gomes@paretogroup.com.br>
2020-05-11 00:49:04 -0300

    Git

---
+++ TheComputersmm/src/test/java/com/example/TheComputersmm/TheComputersmmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TheComputersmm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheComputersmmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
