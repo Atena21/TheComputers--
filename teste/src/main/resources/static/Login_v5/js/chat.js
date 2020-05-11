785d706d4a4362ea4b4bf4401963e8b902859d31
SarahVeldhuis <sarahmveldhuis@gmail.com>
2020-05-11 04:15:38 -0300

    add

--- teste/src/main/resources/static/Login_v5/js/chat.js
+++ teste/src/main/resources/static/Login_v5/js/chat.js
@@ -1,8 +1,12 @@
-function loadArray(){
-    var text1 = {user : "me", message: "oiiiiiiii"};
-    var text2 = {user : "other", message: "Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola "};
-    var array = [text1, text2, text1, text2];
+var text1 = {user : "me", message: "oiiiiiiii"};
+var text2 = {user : "other", message: "Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola Ola "};
+var array_original = [text1, text2, text1, text2];
 
+//array_original são as mensagens do histórico
+var array_novo=[];
+
+
+function loadArray(array){
     array.forEach(function(item) {
     var mdiv = document.createElement("div");
     var div = document.createElement("div")
@@ -19,4 +23,20 @@ function loadArray(){
     mdiv.cli
     document.getElementById("chats").appendChild(mdiv);
    });
+
+}
+
+function loadText(){
+    var text = document.getElementById("text").value;
+    console.log(text);
+    document.getElementById('text').value="";
+    document.getElementById('text').placeholder= "New message...";
+    var textme = {user: "me", message: text};
+    array_novo=[textme]
+
+    //tem que pegar daqui pra ir pro bd(array_novo)
+    
+    if(text != ""){
+        loadArray(array_novo);
+    }
 }
